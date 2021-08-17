f4513989ce87ebd06d98922e495a19a6d0085433
Mitter <f.mitter@accenture.com>
2021-08-17 17:08:04 +0200

    Navbar completed

---
+++ project/react-project/src/components/Navbar.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import logo from '../img.jpg'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className="container">
+        <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo"/></a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+       
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Courses
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Registration</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Gallery</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Blog</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Help</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contact Us</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+        </div>
+        </div>
+      </nav>
+    )
+}
+
+export default Navbar
